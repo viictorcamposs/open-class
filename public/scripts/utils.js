02f8444752b881ea6056a0e115d944f697cd413b
Victor Campos <campos.viictor@gmail.com>
2020-07-20 13:58:22 -0300

    finalizando plataforma de cadastro de professores e alunos

---
+++ public/scripts/utils.js
@@ -0,0 +1,34 @@
+
+exports.age = function ( timestamp ) {
+  const today = new Date ()
+  const birthDate = new Date ( timestamp )
+  let age = today.getFullYear () - birthDate.getFullYear ()
+  const month = today.getMonth () - birthDate.getMonth ()
+  if (month < 0 || month == 0 && today.getDate () < birthDate.getDate ()) age = age - 1
+  return age
+}
+exports.date = function ( timestamp ) {
+  const date = new Date ( timestamp )
+
+  const year = date.getUTCFullYear ()
+  const month = `0${date.getUTCMonth () + 1}`.slice (-2)
+  const day = `0${date.getUTCDate ()}`.slice (-2)
+
+  return {
+    day,
+    month,
+    year,
+    iso: `${year}-${month}-${day}`,
+    birthDay: `${day}/${month}`
+  }
+}
+exports.grade = function ( grade ) {
+  if (grade == '5f') return '5º ano do ensino fundamental'
+  if (grade == '6f') return '6º ano do ensino fundamental'
+  if (grade == '7f') return '7º ano do ensino fundamental'
+  if (grade == '8f') return '8º ano do ensino fundamental'
+  if (grade == '9f') return '9º ano do ensino fundamental'
+  if (grade == '1m') return '1º ano do ensino médio'
+  if (grade == '2m') return '2º ano do ensino médio'
+  if (grade == '3m') return '3º ano do ensino médio'
+}
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require ('express') // Require --> requisição para chamar dependência, arquivo, etc
 const nunjucks = require ('nunjucks')
 const routes = require ('./routes')
+const methodOverride = require ('method-override')
+
 const server = express ()
 
+server.use (express.urlencoded ({ extended:true })) // linha responsável por habilitar o uso do req.body
 server.use (express.static ('public/styles'))
+server.use (express.static ('public/scripts'))
+server.use (methodOverride('_method'))
 server.use (routes)
 
 server.set ('view engine', 'njk')
